4c09f4a86a405d9474d82cf016d6e460c777ce8a
humbertodrc <humbertorivero9@gmail.com>
2023-04-12 08:44:52 -0300

    Add query from country

---
+++ src/querys/index.js
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const GET_COUNTRIES = gql`
+  query GetCountries {
+    countries {
+      name
+      capital
+      currency,
+      languages {
+        name
+      }
+    }
+  }
+`
